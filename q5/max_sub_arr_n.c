f30852f157d5e0e3f1e58940ae1af4d01856f4e6
Oriya-Sigawy <oriyas.07@gmail.com>
2024-04-26 14:48:59 +0300

    initial solution for q5

---
+++ q5/max_sub_arr_n.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int x, int y)
+{
+    return x > y ? x : y;
+}
+
+void create_arr(int *arr, unsigned int seed, int size)
+{
+    srand(seed);
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 100 - 25;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int size = argv[2] - '0';
+    int * array = (int *)malloc(size * sizeof(int));
+    unsigned int seed = argv[1] - '0';
+    create_arr(array, seed, size);
+    int best = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        for (size_t j = i; j < size; j++)
+        {
+            int sum = 0;
+            for (size_t k = i; k <= j; k++)
+            {
+                sum += array[k];
+            }
+            best = max(best, sum);
+        }
+    }
+    printf("the result is %d", best);
+    return 0;
+}
---
+++ q5/max_sub_arr_n3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int x, int y)
+{
+    return x > y ? x : y;
+}
+
+void create_arr(int *arr, unsigned int seed, int size)
+{
+    srand(seed);
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 100 - 25;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int size = argv[2] - '0';
+    int *array = (int *)malloc(size * sizeof(int));
+    unsigned int seed = argv[1] - '0';
+    create_arr(array, seed, size);
+    int best = 0, sum = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        sum = max(array[i], sum + array[i]);
+        best = max(best, sum);
+    }
+    printf("the result is %d", best);
+    return 0;
+}
